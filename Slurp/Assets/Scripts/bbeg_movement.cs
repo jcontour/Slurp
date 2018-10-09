53c9949a415b10e713cb0cdaf7cffd15123dc5be
jcontour <contj186@newschool.edu>
2018-10-09 15:09:39 -0400

    jumping movement

--- Slurp/Assets/Scripts/bbeg_movement.cs
+++ Slurp/Assets/Scripts/bbeg_movement.cs
@@ -4,43 +4,62 @@ using UnityEngine;
 
 public class bbeg_movement : MonoBehaviour {
 
-	public float Timer = 2f;
+	public float Timer = 0f;
 	public GameObject target;
 	float attraction = 0;
 	int minX, maxX, minZ, maxZ;
-	// Use this for initialization
+	Vector3 bbegStart, bbegEnd;
+
 	void Start () {
-		
+		bbegStart = transform.position;
+		bbegEnd = transform.position;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		Timer -= Time.deltaTime;
-		if (Timer <= 0) 
-		{
-			transform.position = transform.position + moveTo(target.transform.position, transform.position); 
-			Timer = 2f;
-		}
+
+		Timer += Time.deltaTime;
+
+		if (Timer >= 2) {
+			bbegStart = transform.position;
+			bbegEnd = moveTo (transform.position, target.transform.position);
+			Timer = 0f;
+		} 
+
+		jumpBtwnPoints (bbegStart, bbegEnd, Timer);
 
 	}
 
-	Vector3Int moveTo(Vector3 end, Vector3 start){
+	Vector3Int moveTo(Vector3 start, Vector3 end){
 		Vector3 dist = end - start;
 		int amount = Mathf.RoundToInt(dist.magnitude);
 		if (amount > 5) { attraction = .5f; } else { attraction = 1; }
-		if (dist.x < 0 || dist.z < 0) {
+
+		if (Mathf.RoundToInt(dist.x) < 0) {
 			minX = Mathf.RoundToInt(dist.x * attraction);
 			maxX = 0;
+		} else {
+			minX = 0;
+			maxX = Mathf.RoundToInt(dist.x * attraction);
+		}
+
+		if (Mathf.RoundToInt(dist.z) < 0) {
 			minZ = Mathf.RoundToInt(dist.z * attraction);
 			maxZ = 0;
 		} else {
-			minX = Mathf.RoundToInt(dist.x * attraction);
-			maxX = 0;
-			minZ = Mathf.RoundToInt(dist.z * attraction);
-			maxZ = 0;
+			minZ = 0;
+			maxZ = Mathf.RoundToInt(dist.z * attraction);
 		}
 
-		return new Vector3Int (Random.Range (minX, maxX), 0, Random.Range (minZ, maxZ));
+		Vector3Int movingTo = new Vector3Int (Mathf.RoundToInt(start.x + Random.Range (minX, maxX)), 1, Mathf.RoundToInt(start.z + Random.Range (minZ, maxZ)));
+		return movingTo;
+	}
+
+	void jumpBtwnPoints (Vector3 start, Vector3 end, float timer){
+
+		Vector3 peakPoint = new Vector3(start.x+(end.x - start.x) /2, 5, start.z+(end.z - start.z) /2);
+		Vector3 m1 = Vector3.Lerp (start, peakPoint, timer);
+		Vector3 m2 = Vector3.Lerp (peakPoint, end, timer);
+		transform.position = Vector3.Lerp (m1, m2, timer);
 
 	}
 		
--- Slurp/Assets/Scripts/player_movement.cs
+++ Slurp/Assets/Scripts/player_movement.cs
@@ -4,24 +4,56 @@ using UnityEngine;
 
 public class player_movement : MonoBehaviour {
 
-	// Use this for initialization
+	Vector3 startPos, endPos;
+	float Timer = 0;
+	bool doMove = false;
+	public float speed = 10;
+
 	void Start () {
-		
+		startPos = transform.position;
+		endPos = transform.position;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			transform.position = new Vector3((transform.position.x - 1f), transform.position.y, transform.position.z);
-		}
-		if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			transform.position = new Vector3((transform.position.x + 1f), transform.position.y, transform.position.z);
-		}
-		if (Input.GetKeyDown(KeyCode.RightArrow)) {
-			transform.position = new Vector3(transform.position.x, transform.position.y, (transform.position.z + 1f));
-		}
-		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-			transform.position = new Vector3(transform.position.x, transform.position.y, (transform.position.z - 1f));
+		if (doMove) {
+			Timer += Time.deltaTime * speed;
+			if (Timer < 1.5f) {
+				movePlayer (startPos, endPos, Timer);
+			} else {
+				doMove = false;
+				Timer = 0;
+			}
+
+		} else {
+			if (Input.GetKeyDown (KeyCode.UpArrow)) {
+				startPos = transform.position;
+				endPos = new Vector3 ((transform.position.x - 1f), transform.position.y, transform.position.z);
+				doMove = true;
+			}
+			if (Input.GetKeyDown (KeyCode.DownArrow)) {
+				startPos = transform.position;
+				endPos = new Vector3 ((transform.position.x + 1f), transform.position.y, transform.position.z);
+				doMove = true;
+			}
+			if (Input.GetKeyDown (KeyCode.RightArrow)) {
+				startPos = transform.position;
+				endPos = new Vector3 (transform.position.x, transform.position.y, (transform.position.z + 1f));
+				doMove = true;
+			}
+			if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+				startPos = transform.position;
+				endPos = new Vector3 (transform.position.x, transform.position.y, (transform.position.z - 1f));
+				doMove = true;
+			}
 		}
 	}
+
+	void movePlayer (Vector3 start, Vector3 end, float timer){
+			Vector3 peakPoint = new Vector3(start.x+(end.x - start.x) /2, 1.5f, start.z+(end.z - start.z) /2);
+			Vector3 m1 = Vector3.Lerp (start, peakPoint, timer);
+			Vector3 m2 = Vector3.Lerp (peakPoint, end, timer);
+			transform.position = Vector3.Lerp (m1, m2, timer);
+	}
+
 }
+
